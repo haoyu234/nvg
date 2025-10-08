14f58173d39811a16d1c22dc85f0bbdcaa5cb30f
握猫猫 <164346864@qq.com>
2025-10-08 12:55:49 +0800

    优化 pieces 避免字段与方法重名

--- src/nvg/pieces.nim
+++ src/nvg/pieces.nim
@@ -1,49 +1,48 @@
 type Piece*[T] = object
   storage: ptr UncheckedArray[T]
-  len: int
+  dataLen: int32
 
 template `^^`(s, i: untyped): untyped =
-  (when i is BackwardsIndex: s.len - int(i) else: int(i))
+  (when i is BackwardsIndex: s.dataLen - int32(i) else: int32(i))
 
 proc len*[T](s: Piece[T]): int {.inline.} =
-  s.len
+  int(s.dataLen)
 
-proc piece*[T](p: ptr UncheckedArray[T], len: int): Piece[T] {.inline, raises: [].} =
-  Piece[T](storage: p, len: len)
+proc piece*[T](p: ptr UncheckedArray[T], len: Natural): Piece[T] {.inline, raises: [].} =
+  Piece[T](storage: p, dataLen: int32(len))
 
 proc piece*[T](s: openArray[T]): Piece[T] {.inline, raises: [].} =
   if s.len <= 0:
     return
 
-  Piece[T](storage: cast[ptr UncheckedArray[T]](s[0].addr), len: s.len)
+  Piece[T](storage: cast[ptr UncheckedArray[T]](s[0].addr), dataLen: int32(s.len))
 
 proc piece*[T](s: var openArray[T]): Piece[T] {.inline, raises: [].} =
   if s.len <= 0:
     return
 
-  Piece[T](storage: cast[ptr UncheckedArray[T]](s[0].addr), len: s.len)
-
-proc piece*[T](s: var seq[T]): Piece[T] {.inline, raises: [].} =
-  if s.len <= 0:
-    return
-
-  Piece[T](storage: cast[ptr UncheckedArray[T]](s[0].addr), len: s.len)
+  Piece[T](storage: cast[ptr UncheckedArray[T]](s[0].addr), dataLen: int32(s.len))
 
 template toOpenArray*[T](s: Piece[T]): openArray[T] =
-  s.storage.toOpenArray(0, s.len - 1)
+  s.storage.toOpenArray(0, s.dataLen - 1)
 
 template toOpenArray*[T](s: Piece[T], i: Natural): openArray[T] =
-  s.storage.toOpenArray(i, s.len - 1)
+  assert int32(i) < s.dataLen
+
+  s.storage.toOpenArray(i, s.dataLen - 1)
 
 template toOpenArray*[T](s: Piece[T], i, j: Natural): openArray[T] =
+  assert int32(i) <= int32(j)
+  assert int32(j) < s.dataLen
+
   s.storage.toOpenArray(i, j)
 
 proc `[]`*[T; L, H: Ordinal](s: Piece[T], x: HSlice[L, H]): Piece[T] {.inline.} =
   let a = s ^^ x.a
   let L = (s ^^ x.b) - a + 1
 
-  assert L <= s.len
-  assert (a + L) <= s.len
+  assert L <= s.dataLen
+  assert (a + L) <= s.dataLen
 
   if L <= 0:
     return
@@ -51,17 +50,17 @@ proc `[]`*[T; L, H: Ordinal](s: Piece[T], x: HSlice[L, H]): Piece[T] {.inline.} 
   piece(cast[ptr UncheckedArray[T]](s.storage[a].addr), L)
 
 proc `[]`*[T](s: Piece[T], i: Natural): var T {.inline.} =
-  assert int(i) < s.len
+  assert int32(i) < s.dataLen
 
   s.storage[i]
 
 proc `[]`*[T](s: Piece[T], i: BackwardsIndex): var T {.inline.} =
-  let i = s.len - int(i)
-  assert int(i) < s.len
+  let i = s.dataLen - int32(i)
+  assert i < s.dataLen
 
   s.storage[i]
 
 proc `[]=`*[T](s: Piece[T], i: Natural, v: sink T) {.inline.} =
-  assert int(i) < s.len
+  assert int32(i) < s.dataLen
 
   s.storage[i] = v
