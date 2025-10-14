7e25f3acfc95336bc602f7ad29e84c6ad49fc1f5
haoyu234 <164346864@qq.com>
2025-10-14 21:29:20 +0800

    字体文件移动到资源文件夹

--- tests/fonts.nim
+++ tests/fonts.nim
@@ -2,9 +2,9 @@ import nvg/context
 import nvg/core
 
 const
-  FONT = staticRead("../vivoSans-Regular.ttf")
-  FONT_mono = staticRead("../MapleMono.ttf")
-  FONT_emoji = staticRead("../OpenMoji-color-glyf_colr_0.ttf")
+  FONT = staticRead("../assets/vivoSans-Regular.ttf")
+  FONT_mono = staticRead("../assets/MapleMono.ttf")
+  FONT_emoji = staticRead("../assets/OpenMoji-color-glyf_colr_0.ttf")
 
 var
   fontId = default(FontId)
